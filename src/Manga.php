48437c96f9c4db27878baeec0e4d0e1c2e42ffc2
lord_MATATA <s.mtz.hakim@gmail.com>
2020-09-24 20:44:38 +0330

    fix some bugs

--- src/Manga.php
+++ src/Manga.php
@@ -5,7 +5,16 @@ use MangaCrawlers\MangaPanda;
 
 class Manga
 {
-    private $error;
+    private $error = ["message" => ""];
+
+    public function remove_cache_files($_chapter_path)
+    {
+        for ($i = 1; file_exists($_chapter_path . $i . '.jpg'); $i++) {
+            if (!unlink($_chapter_path . $i . '.jpg')) {
+                $this->error["message"] .= $i . " problem in path unlink " . $_chapter_path;
+            }
+        }
+    }
 
     public function downloader(string $_crawler, string $_manga, int $_chapter, string $_path)
     {
@@ -18,18 +27,14 @@ class Manga
                     return false;
                 }
                 if (!$this->pdf_maker($_manga, $_chapter, $chapter_path)) {
+                    $this->remove_cache_files($chapter_path);
                     return false;
                 }
                 if (!$this->zip_maker($_manga, $_chapter, $chapter_path)) {
+                    $this->remove_cache_files($chapter_path);
                     return false;
                 }
-
-                $this->error["message"] = "";
-                for ($i = 1; file_exists($chapter_path . $i . '.jpg'); $i++) {
-                    if (!unlink($chapter_path . $i . '.jpg')) {
-                        $this->error["message"] .= $i . " problem in path unlink " . $chapter_path;
-                    }
-                }
+                $this->remove_cache_files($chapter_path);
 
                 return true;
         }
@@ -46,8 +51,10 @@ class Manga
             $im = new \Imagick($image_list);
             $im->setImageFormat('pdf');
             $im->writeImages($_chapter_path . $_manga . "_" . $_chapter . ".pdf", true);
+            return true;
         } catch (\ImagickException $th) {
             $this->error["message"] = $th->getMessage();
+            return false;
         }
     }
 
@@ -57,6 +64,7 @@ class Manga
         $job = $zip->open($_chapter_path . $_manga . "_" . $_chapter . ".zip", \ZipArchive::CREATE);
         if (!$job) {
             $this->error["message"] = $job;
+            return false;
         }
 
         for ($i = 1; file_exists($_chapter_path . $i . '.jpg'); $i++) {
@@ -64,5 +72,11 @@ class Manga
         }
 
         $zip->close();
+        return true
+    }
+
+    public function get_error()
+    {
+        return $this->error;
     }
 }
--- tests/MangaTest.php
+++ tests/MangaTest.php
@@ -9,7 +9,8 @@ final class MangaTest extends TestCase
     public function testDownloader()
     {
         $obj = new Manga();
-        $result = $obj->downloader("mangapanda", "bleach", 6, "up");
+        $result = $obj->downloader("mangapanda", "bleach", 11, __DIR__ . "/up/");
+        var_dump($obj->get_error());
         $this->assertTrue($result);
     }
 }
