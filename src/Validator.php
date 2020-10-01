68c6b223875b7db2883231835a8230fecd17c8b2
lord_MATATA <s.mtz.hakim@gmail.com>
2020-10-01 18:39:30 +0330

    fix the manga checker and chapter checker

--- src/Validator.php
+++ src/Validator.php
@@ -23,30 +23,51 @@ class Validator
             $url = "http://www.mangapanda.com/$_manga";
 
             $html = file_get_contents($url);
-            if (!$html) {
+            if (empty($html)) {
                 $this->error["message"] = "manga input error";
                 return false;
             }
             return true;
         }
-        return true;
+        return false;
     }
 
     public function check_chapter(string $_crawler, string $_manga, int $_chapter)
     {
-        // if ($_crawler == "mangapanda") {
-        //     $url = "http://www.mangapanda.com/$_manga/$_chapter";
+        if ($_crawler == "mangapanda") {
+            $html = "http://www.mangapanda.com/$_manga/$_chapter";
+            $image_url = $this->get_inner_string($html, 'id="img"', 'alt=');
+            $image_url = substr($image_url, strpos($image_url, 'https://'));
+            $image_url = substr($image_url, 0, strpos($image_url, '"'));
 
-        //     $html = file_get_contents($url);
-        //     if (!$html) {
-        //         $this->error["message"] = "manga or chapter input error";
-        //         return false;
-        //     }
-        //     return true;
-        // }
-        // $this->error["message"] = "crawler input error";
-        // return false;
-        return true;
+            if (strlen($image_url) > 10) {
+                return $image_url;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private function get_inner_string($string, $begin, $end)
+    {
+        if ($begin === 0) {
+            return substr($string, 0, strpos($string, $end));
+        }
+        if ($end === 0) {
+            return substr($string, strpos($string, $begin) + strlen($begin));
+        }
+
+        $string = ' ' . $string;
+        $init = strpos($string, $begin);
+
+        if ($init == 0) {
+            return '';
+        }
+
+        $init += strlen($begin);
+        $len = strpos($string, $end, $init) - $init;
+
+        return substr($string, $init, $len);
     }
 
     public function get_error()
